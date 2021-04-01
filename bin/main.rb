58ee98938e9246a5443f1ac1b242071bd585c5d3
Amalcxc <amalcxirsi@gmail.com>
2021-04-02 01:06:16 +0300

    compact the title with date and type in arrays

--- bin/main.rb
+++ bin/main.rb
@@ -1,26 +1,8 @@
-require 'nokogiri'
-require 'open-uri'
+require_relative '../lib/logic.rb'
+
+scrapar = Scrapar.new
+
+puts scrapar.scapering
 
 
-def scapering
-  url = "https://www3.bflix.to/home"
-  html = open("#{url}").read
-  nokogiri_doc = Nokogiri::HTML(html)
-  movie_listings = nokogiri_doc.css('div.item')
-  movies = []
-
-  movie_listings.each do |movie_listing|
-    movie = {
-      title: movie_listing.css('.info h3').text,
-      date: movie_listing.css('.info span').text,
-      url: "https://www3.bflix.to" + movie_listing.css('a')[0].attributes["href"].value
-    }
-    movies << movie
-      
-  end
-  puts movies
-  puts movies.count
-end
-
-scapering
 
