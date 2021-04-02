8c54e7c766d21cf0ef68cb3236579a82e49b05fc
Amalcxc <amalcxirsi@gmail.com>
2021-04-02 20:43:23 +0300

    fixe linter errorsname: Tests

--- spec/tast_spec.rb
+++ spec/tast_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../lib/logic'
 
 describe Scraper do
-  let(:film) {Scraper.new}
-  let(:films) {film.scrapering}
+  let(:film) { Scraper.new }
+  let(:films) { film.scrapering }
   describe '#scrapering' do
     it 'return false if the array is not empty' do
       expect(films.empty?).to eql(false)
@@ -12,4 +12,4 @@ describe Scraper do
       expect(films.empty?).to_not eql(true)
     end
   end
-end 
+end
