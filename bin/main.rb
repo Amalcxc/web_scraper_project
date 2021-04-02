36691753df5797dc3a1b2a599fa19bece109cb6b
Amalcxc <amalcxirsi@gmail.com>
2021-04-02 23:41:21 +0300

    filters between tv and movies

--- bin/main.rb
+++ bin/main.rb
@@ -3,17 +3,27 @@ require_relative '../lib/logic'
 scraper = Scraper.new
 scraper.scrapering
 
+
 title = scraper.movies
 types = scraper.movies_quality
 date = scraper.movies_date
 
-puts 'choose a movie type'
-input = gets.chomp
-
 types.each_with_index do |element, index|
-  scraper.new_quality << index if element == input
+  scraper.new_quality << index if element == 'TV'
 end
 scraper.new_quality.each do |index|
   puts "- - - index: #{index + 1} - - -"
   puts "title: #{title[index]} | types: #{types[index]} | date: #{date[index][0..4]}"
 end
+
+# puts "choose a movie type"
+# input = gets.chomp
+
+# types.each_with_index do |element, index|
+#   scraper.new_quality << index if element == input
+# end
+# scraper.new_quality.each do |index|
+#   puts "- - - index: #{index + 1} - - -"
+#   puts "title: #{title[index]} | types: #{types[index]} | date: #{date[index][0..4]}"
+# end
+
