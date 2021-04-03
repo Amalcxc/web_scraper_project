282da35ddf24f78ba5a0e448b8b6fe04366fdc07
Amalcxc <amalcxirsi@gmail.com>
2021-04-03 20:52:47 +0300

    Fixe linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -9,24 +9,22 @@ date = scraper.movies_date
 
 puts title[0..11]
 
-puts "Use Quit To Stop The Loop"
+puts 'Use Quit To Stop The Loop'
 
 while scraper.scrapering
   puts 'Choose A Movie/Show Name:'
   input = gets.chomp.downcase
   scraper.new_title = []
-  break if input == "quit" 
+  break if input == 'quit'
 
   title.each_with_index do |element, index|
     scraper.new_title << index if element.include?(input)
   end
 
   puts 'No Movie with That Name' if scraper.new_title.empty?
-  
+
   scraper.new_title.each do |index|
     puts "- - - index: #{index + 1} - - -"
     puts "title: #{title[index]} | types: #{types[index]} | date/season: #{date[index][0..4]}"
   end
-end  
-
-
+end
